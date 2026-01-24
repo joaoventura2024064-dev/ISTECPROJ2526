6554f9f23d3beef909d187cd65231e07e0c0eab0
João Ventura <joao.ventura.2024064@my.istec.pt>
2026-01-24 23:23:47 +0000

    fix: Fixed badge colors

--- frontend/src/components/common/Badge.jsx
+++ frontend/src/components/common/Badge.jsx
@@ -4,20 +4,12 @@ function Badge({ text, variant = 'default', size = 'medium', border = false, cla
 
     const base = `inline-flex items-center rounded-full text-xs font-medium inset-ring  ${size === 'medium' ? "px-2 py-1" : "px-1.5 py-0.5"} ${border ? "border border-base-600" : ""} pointer-events-none`;
 
-    const colors = {
-        default: "base-800",
-        secondary: "secondary-500",
-        destructive: "red-500",
-        success: "primary-500",
-        outline: "neutral-950"
-    }
-
     const variants = {
-        default: `bg-${colors.default}/10 text-${colors.default} inset-ring-${colors.default}/20`,
-        secondary: `bg-${colors.secondary}/10 text-${colors.secondary} inset-ring-${colors.secondary}/20`,
-        destructive: `bg-${colors.destructive}/10 text-${colors.destructive} inset-ring-${colors.destructive}/20`,
-        success: `bg-${colors.success}/10 text-${colors.success} inset-ring-${colors.success}/20`,
-        outline: `text-${colors.outline}`,
+        default: "bg-base-800/10 !text-base-800 inset-ring-base-800/20",
+        secondary: "bg-secondary-500/10 !text-secondary-500 inset-ring-secondary-500/20",
+        destructive: "bg-red-500/10 !text-red-500 inset-ring-red-500/20",
+        success: "bg-primary-500/10 !text-primary-500 inset-ring-primary-500/20",
+        outline: "text-neutral-300",
     };
 
     return (
